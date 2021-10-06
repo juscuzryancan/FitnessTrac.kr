5aeb88b6b13f5eb91fe830513dea95f5188886dc
Ryan Riley Puzon <rrgpuzon@gmail.com>
2021-10-06 01:30:00 -0700

    edits client to have rejectUnauth to be false

--- db/client.js
+++ db/client.js
@@ -1,5 +1,10 @@
 // build and export your unconnected client here
 const { Client } = require('pg');
-const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/fitness-dev');
+const client = new Client({
+	connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/fitness-dev',
+	ssl: {
+		rejectUnauthorized: false
+	}
+});
 
 module.exports = client;
