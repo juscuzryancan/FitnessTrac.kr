1e00eeee7d33451c76526d22fed28918babad653
Ryan Riley Puzon <rrgpuzon@gmail.com>
2021-12-30 14:31:48 -0800

    updates to latest react-router and updates client

--- db/client.js
+++ db/client.js
@@ -2,9 +2,7 @@
 const { Client } = require('pg');
 const client = new Client({
 	connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/fitness-dev',
-	ssl: {
-		rejectUnauthorized: false
-	}
+	ssl: process.env.DATABASE_URL ? true : false
 });
 
 module.exports = client;
