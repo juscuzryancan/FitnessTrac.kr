7acd760da67096e626a3b253dd69f999fa8408a3
Ryan Riley Puzon <rrgpuzon@gmail.com>
2022-01-04 23:27:04 -0800

    client change

--- db/client.js
+++ db/client.js
@@ -2,7 +2,7 @@
 const { Client } = require('pg');
 const client = new Client({
 	connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/fitness-dev',
-	ssl: process.env.DATABASE_URL ? true : false
+	ssl: { rejectUnauthorized: false }
 });
 
 module.exports = client;
