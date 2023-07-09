cdcc88481ef6ea0aea4f585639b45f871e351231
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-07-09 16:57:01 -0700

    prevents negative values within inputs

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Loader } from ".";
 import { useToken } from "../contexts/useToken";
 import { useMutation, useQueries, useQueryClient } from "react-query";
 import { deleteActivity, editCountOrDuration } from "../api";
@@ -33,7 +34,7 @@ const Activity = ({
     }
   }); 
 
-  const { mutate: handleDelete } = useMutation({
+  const { mutate: handleDelete, isLoading } = useMutation({
     mutationFn: () => deleteActivity(token, routineActivityId),
     onSuccess: () => {
       queryClient.invalidateQueries(["routines"]);
@@ -50,15 +51,35 @@ const Activity = ({
       {isOwner && <>
         <div className="flex gap-1">
           <label>Count</label>
-          <input className="border rounded border-black" value={count} onChange={(e)=> setCount(e.target.value)} type="number"/>
+          <input 
+            className="border rounded border-black" 
+            min="0" 
+            value={count} 
+            onChange={(e)=> {
+              if (e.target.value < 0) {
+                setCount(Math.abs(e.target.value));
+              } else {
+                setCount(e.target.value)
+              }
+            }} 
+            type="number"
+          />
         </div>
         <div className="flex gap-1">
           <label>Duration</label>
-          <input className="border rounded border-black" value={duration} onChange={(e)=> setDuration(e.target.value)} type="number"/>
+          <input className="border rounded border-black" min="0" value={duration} 
+            onChange={(e)=> {
+              if (e.target.value < 0) {
+                setDuration(Math.abs(e.target.value));
+              } else {
+                setDuration(e.target.value)
+              }
+            }}
+            type="number"/>
         </div>
         <div className="flex justify-end gap-4 mx-2">
           <button className="border rounded border-black py-2 px-4 bg-blue-200" onClick={handleEdit}>Save Activity</button>
-          <button className="border rounded border-black py-2 px-4 bg-red-400" onClick={handleDelete}>Remove Activity</button>
+          <button className="border rounded border-black py-2 px-4 bg-red-400" onClick={handleDelete}>{isLoading ? <Loader/> : "Remove Activity"}</button>
         </div>
       </>}
     </div>
